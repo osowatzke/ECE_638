014ed6197abc63fa22b47af299ee4654702f2aa1
Owen <osowatzke@gmail.com>
2024-12-13 00:05:41 -0700

    Debugging

--- Final/getSymbolErrorProbability.m
+++ Final/getSymbolErrorProbability.m
@@ -1,24 +1,28 @@
-N = 5e6;
+N = 1e6;
 SNR_dB = 0:40;
 pError = zeros(size(SNR_dB));
-txSymbols = randi([0 16], N, 1);
+txSymbols = randi([0 8], N, 1);
 txSymbols(txSymbols>=8) = 8;
 txSignal = exp(1i*2*pi*txSymbols/8).*(txSymbols<8);
+avgPwr = mean(abs(txSignal).^2);
+txSignal = txSignal/sqrt(avgPwr);
 for i = 1:length(SNR_dB)
-    rxSignal = awgn(txSignal, SNR_dB(i) + 10*log10(2));
+    rxSignal = awgn(txSignal, SNR_dB(i));
+%     noise = 10^(-SNR_dB(i)/20)*complex(randn(size(txSignal)),randn(size(txSignal)))/sqrt(2);
+%     rxSignal = txSignal + noise;
     rArray = 0.5;
     r = 0.5;
     isZero = (abs(rxSignal) < r);
     rxSymbols = mod(round(angle(rxSignal)*8/(2*pi)),8);
     rxSymbols(isZero) = 8;
-    pError(i) = mean(txSymbols ~= rxSymbols);
+    pError(i) = mean(txSymbols ~= rxSymbols & txSymbols == 8);
 end
 figure(1)
 clf;
 semilogy(SNR_dB, pError)
 Q = @(x)normcdf(-x);
 p = 10.^(SNR_dB/10);
-pErrorEst = 4*Q(sqrt(p)) + Q(2*sqrt(p)*sin(pi/8));
+pErrorEst = Q(sqrt(p/2)); % + Q(2*sqrt(2*p)*sin(pi/8));
 hold on;
 semilogy(SNR_dB, pErrorEst);
 grid on;
