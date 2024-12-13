ba35840dd36e783dd1387585a50f12340813a5f5
Owen <osowatzke@gmail.com>
2024-12-13 02:19:10 -0700

    Saving work before proceeding

--- Final/getSymbolErrorProbability.m
+++ Final/getSymbolErrorProbability.m
@@ -1,5 +1,5 @@
 % Number of samples to generate
-N = 1e6;
+N = 5e6;
 
 % Range of SNR to sweep
 SNR_dB = 0:20;
@@ -18,23 +18,18 @@ txSignal = exp(1i*2*pi*txSymbols/8).*(txSymbols<8);
 avgPwr = mean(abs(txSignal).^2);
 txSignal = txSignal/sqrt(avgPwr);
 
+% Get constellation points for comparison
+si = [1/sqrt(avgPwr)*exp(1i*2*pi*(0:7)/8), 0];
+
 % Loop for each SNR
 for i = 1:length(SNR_dB)
 
     % Pass signal through AWGN channel
     rxSignal = awgn(txSignal, SNR_dB(i));
-%     noise = 10^(-SNR_dB(i)/20)*complex(randn(size(txSignal)),randn(size(txSignal)))/sqrt(2);
-%     rxSignal = txSignal + noise;
-    
-    % Demodulate symbol
-    % Assume optimal decision threshold is 0.5
-    % Should be true for high SNR
-    r = 0.5;
-    isZero = (abs(rxSignal) < r);
 
     % Demodulate symbols
-    rxSymbols = mod(round(angle(rxSignal)*8/(2*pi)),8);
-    rxSymbols(isZero) = 8;
+    [~,rxSymbols] = min(abs(rxSignal.'-si.'));
+    rxSymbols = rxSymbols.'-1;
 
     % Determine the probability of error
     pError(i) = mean(txSymbols ~= rxSymbols);
@@ -51,7 +46,7 @@ title('Probability of Symbol Error in AWGN Channel')
 % Estimate probability of symbol error
 Q = @(x)normcdf(-x);
 p = 10.^(SNR_dB/10);
-pErrorEst = 4*Q(sqrt(p/2)) + Q(2*sqrt(p)*sin(pi/8));%0.5*exp(-p/4) + Q(2*sqrt(p)*sin(pi/8));
+pErrorEst = 4*Q(sqrt(p)) + Q(2*sqrt(p)*sin(pi/8));
 
 % Plot estimate
 hold on;
